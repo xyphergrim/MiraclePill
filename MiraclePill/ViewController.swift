1d69a3ec367bad4779991457ea937966e26f733c
xyphergrim <johnclancy@outlook.com>
2016-11-15 14:44:11 -0600

    Lecture 44
    Added views to the UI - labels and text fields and added constraints

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
